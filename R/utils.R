846138b644145b9f1a2920357e485b7a92a3feda
Jim Hester <james.f.hester@gmail.com>
2017-09-20 10:40:17 -0400

    Import glue functions

--- R/utils.R
+++ R/utils.R
@@ -2,7 +2,7 @@ choices_rd <- function(x) {
   paste0(collapse = ", ", paste0("\\sQuote{", x, "}")) # nocov
 }
 
-#' @importFrom glue collapse single_quote
+#' @importFrom glue collapse single_quote evaluate
 collapse_quote_transformer <- function(code, envir, data) {
   collapse_re <- "[*]$"
   quote_re <- "^[|]"

20b05829b78b7b6f739b0af5339f85c8527885f1
Jim Hester <james.f.hester@gmail.com>
2021-06-08 12:15:09 -0400

    Convert write_file.cpp

--- src/write_file.cpp
+++ src/write_file.cpp
@@ -1,5 +1,4 @@
 #include "r_archive.h"
-#include <Rcpp.h>
 #include <fcntl.h>
 
 #if ARCHIVE_VERSION_NUMBER >= 3002000
@@ -69,10 +68,10 @@ void file_write_destroy(Rconnection con) {
 
 // Get a connection to a single non-archive file, optionally with one or more
 // filters.
-[[cpp11::register]] SEXP write_file_connection(
-    const std::string& filename, Rcpp::NumericVector filters) {
+[[cpp11::register]] SEXP
+write_file_connection(const std::string& filename, cpp11::integers filters) {
 #if ARCHIVE_VERSION_NUMBER < 3002000
-  Rcpp::stop("This functionality is only available with libarchive >= 3.2.0");
+  cpp11::stop("This functionality is only available with libarchive >= 3.2.0");
 #else
   Rconnection con;
   SEXP rc = PROTECT(new_connection("input", "wb", "archive_write", &con));
@@ -85,7 +84,7 @@ void file_write_destroy(Rconnection con) {
 
   /* Initialize filters */
   if (filters.size() > FILTER_MAX) {
-    Rcpp::stop("Cannot use more than %i filters", FILTER_MAX);
+    cpp11::stop("Cannot use more than %i filters", FILTER_MAX);
   }
   for (int i = 0; i < FILTER_MAX; ++i) {
     r->filters[i] = -1;
@@ -116,9 +115,9 @@ void file_write_destroy(Rconnection con) {
 // Write files already on disk to a new archive
 [[cpp11::register]] SEXP write_files_(
     const std::string& archive_filename,
-    Rcpp::CharacterVector files,
+    cpp11::strings files,
     int format,
-    Rcpp::NumericVector filters,
+    cpp11::integers filters,
     size_t sz = 16384) {
 
   struct archive* a;
@@ -136,7 +135,7 @@ void file_write_destroy(Rconnection con) {
     Rf_error(archive_error_string(a));
   }
 
-  for (int i = 0; i < filters.length(); ++i) {
+  for (int i = 0; i < filters.size(); ++i) {
     response = archive_write_add_filter(a, filters[i]);
     if (response != ARCHIVE_OK) {
       Rf_error(archive_error_string(a));
@@ -144,13 +143,13 @@ void file_write_destroy(Rconnection con) {
   }
 
   archive_write_open_filename(a, archive_filename.c_str());
-  for (int i = 0; i < files.size(); ++i) {
-    stat(files[i], &st);
+  for (std::string file : files) {
+    stat(file.c_str(), &st);
     entry = archive_entry_new();
     archive_entry_copy_stat(entry, &st);
-    archive_entry_set_pathname(entry, files[i]);
+    archive_entry_set_pathname(entry, file.c_str());
     archive_write_header(a, entry);
-    if ((fd = open(files[i], O_RDONLY)) != -1) {
+    if ((fd = open(file.c_str(), O_RDONLY)) != -1) {
       len = read(fd, buff, sizeof(buff));
       while (len > 0) {
         archive_write_data(a, buff, len);

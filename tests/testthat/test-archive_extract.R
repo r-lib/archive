2723542dc71f55410eed04ef2c35d7d25fc3f803
Jim Hester <james.f.hester@gmail.com>
2021-06-11 10:33:57 -0400

    Move archive_extract to its own files

---
+++ tests/testthat/test-archive_extract.R
@@ -0,0 +1,37 @@
+data_file <- system.file(package = "archive", "extdata", "data.zip")
+
+describe("archive_extract", {
+  it("extracts all files in the archive", {
+    a <- archive(data_file)
+    d <- tempfile()
+    on.exit(unlink(d, recursive = TRUE))
+    archive_extract(a, d)
+
+    f <- list.files(d, full.names = TRUE)
+    expect_equal(length(f), 3)
+    expect_equal(file.size(f), a[order(a[["path"]]), ][["size"]])
+  })
+  it("extracts given files in the archive, indexed by number", {
+    a <- archive(data_file)
+
+    d <- tempfile()
+    on.exit(unlink(d, recursive = TRUE))
+    archive_extract(a, d, file = 1)
+
+    f <- list.files(d, full.names = TRUE)
+    expect_equal(length(f), 1)
+    expect_equal(file.size(f), a[a$path == a$path[[1]], ][["size"]])
+  })
+  it("extracts given files in the archive, indexed by name", {
+    a <- archive(data_file)
+
+    d <- tempfile()
+    on.exit(unlink(d, recursive = TRUE))
+    archive_extract(a, d, file = "mtcars.csv")
+
+    f <- list.files(d, full.names = TRUE)
+    expect_equal(length(f), 1)
+    expect_equal(file.size(f), a[a$path == "mtcars.csv", ][["size"]])
+  })
+})
+

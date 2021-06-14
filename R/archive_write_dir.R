351545e841ea02dc81f4a6e786cc7b75831ca15e
Jim Hester <james.f.hester@gmail.com>
2021-06-14 09:09:40 -0400

    Add options to archiv_write_dir

--- R/archive_write_dir.R
+++ R/archive_write_dir.R
@@ -3,15 +3,19 @@
 #' @param dir `character(1)` The directory of files to add.
 #' @inheritParams base::list.files
 #' @export
-archive_write_dir <- function(archive, dir, ..., recursive = TRUE, full.names = FALSE) {
+archive_write_dir <- function(archive, dir, format = NULL, filter = NULL, options = character(), ..., recursive = TRUE, full.names = FALSE) {
   assert("`dir` {dir} is not readable",
     is_readable(dir))
 
   archive <- file.path(normalizePath(dirname(archive)), basename(archive))
+
+  options <- validate_options(options)
+
   old <- setwd(dir)
   on.exit(setwd(old))
   files <- dir(dir, ..., recursive = recursive, full.names = full.names)
-  archive_write_files(archive, files)
+
+  archive_write_files(archive, files, format = format, filter = filter, options = options)
 
   invisible(as_archive(archive, options = character()))
 }

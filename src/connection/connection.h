4081f65128e0a092aafc5db9a591a0d1c2ec302d
Jim Hester <james.f.hester@gmail.com>
2021-06-14 12:38:34 -0400

    Ignore clang warning

--- src/connection/connection.h
+++ src/connection/connection.h
@@ -2,11 +2,20 @@
 
 #include "Rinternals.h"
 
+// clang-format off
+#ifdef __clang__
+# pragma clang diagnostic push
+# pragma clang diagnostic ignored "-Wkeyword-macro"
+#endif
 #define class class_name
 #define private private_ptr
 #include <R_ext/Connections.h>
 #undef class
 #undef private
+#ifdef __clang__
+# pragma clang diagnostic pop
+#endif
+// clang-format on
 
 #ifdef __cplusplus
 extern "C" {

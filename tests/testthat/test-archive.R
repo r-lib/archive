00bd7a973e70f4e7f3270462814d723bcbb3a035
Jim Hester <james.f.hester@gmail.com>
2021-06-15 15:21:01 -0400

    Skip tests on solaris as well

--- tests/testthat/test-archive.R
+++ tests/testthat/test-archive.R
@@ -11,6 +11,7 @@ describe("archive", {
   })
   it("takes options", {
     skip_on_os("windows")
+    skip_on_os("solaris")
     a <- archive(test_path("cp866.tar.Z.uu"), "hdrcharset=CP866")
     expect_equal(a$path, c("\u041f\u0420\u0418\u0412\u0415\u0422", "\u043f\u0440\u0438\u0432\u0435\u0442"))
   })

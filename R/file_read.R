58e3ff12d0a79a1c012136e2c56f4e2fea688585
Jim Hester <james.f.hester@gmail.com>
2021-07-26 11:27:39 -0400

    Fix tests for file_read

--- R/file_read.R
+++ R/file_read.R
@@ -1,8 +1,13 @@
 #' @rdname file_connections
 #' @export
 file_read <- function(file, mode = "r", filter = NULL, options = character()) {
-  assert("{file} is not a readable file path",
-    is_readable(file))
+  options <- validate_options(options)
 
-  archive_read_(file, file, mode, archive_formats()["raw"], archive_filters()[filter], options, sz = 2^14)
+  if (!inherits(archive, "connection")) {
+    file <- file(file)
+  }
+
+  description <- glue::glue("file_read({desc})", desc = summary(file)$description)
+
+  archive_read_(file, 1L, description, mode, archive_formats()["raw"], archive_filters()[filter], options, sz = 2^14)
 }

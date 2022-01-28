bad2e52060026bf032725588f3d0eb332e6b87b5
Jeroen Ooms <jeroenooms@gmail.com>
2022-01-28 10:36:43 +0100

    Downgrade to 3.3.3 due to performance bug

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Build against static libraries from rwinlib
-if (!file.exists("../windows/libarchive-3.5.2/include")){
+if (!file.exists("../windows/libarchive-3.3.3/include")){
   if (getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/libarchive/archive/v3.5.2.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/libarchive/archive/v3.3.3.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")

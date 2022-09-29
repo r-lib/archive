b08ebe41fee51d827e9936910a9ae96ac7b81732
Pat Schloss <pschloss@umich.edu>
2022-09-28 21:07:55 -0400

    Fix path to libraries to work with windows

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,13 @@
 .onLoad <- function(libname, pkgname) {
-  lib_path <- list.files(paste0(libname, "/archive/lib"), pattern="libconnection.*", full.names=TRUE)
+  lib_path <- list.files(paste0(libname, "/archive/lib/", .Platform$r_arch), pattern="libconnection.*", full.names=TRUE)
+
+print( list.files(paste0(libname, "/archive/lib", .Platform$r_arch) ))
+
   res <- dyn.load(lib_path)
   rchive_init(res$new_connection$address, res$read_connection$address)
 }
 
 .onUnload <- function(libname) {
-  lib_path <- list.files(paste0(libname, "/lib"), pattern="libconnection.*", full.names=TRUE)
+  lib_path <- list.files(paste0(libname, "/lib/", .Platform$r_arch), pattern="libconnection.*", full.names=TRUE)
   dyn.unload(lib_path)
 }

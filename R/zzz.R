afcf70545cda061d47bdd622df3519cfa2fad7a1
Pat Schloss <pschloss@umich.edu>
2022-09-28 21:16:06 -0400

    Remove debugging print statement

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   lib_path <- list.files(paste0(libname, "/archive/lib/", .Platform$r_arch), pattern="libconnection.*", full.names=TRUE)
-
-print( list.files(paste0(libname, "/archive/lib", .Platform$r_arch) ))
-
   res <- dyn.load(lib_path)
   rchive_init(res$new_connection$address, res$read_connection$address)
 }

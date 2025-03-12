f4d3eda16545ff93a9a5f4b55a01a1e63c705782
Michael Chirico <michaelchirico4@gmail.com>
2025-03-12 07:59:40 -0700

    two more hits

--- src/cpp11/include/cpp11/R.hpp
+++ src/cpp11/include/cpp11/R.hpp
@@ -44,7 +44,7 @@
 namespace cpp11 {
 namespace literals {
 
-constexpr R_xlen_t operator"" _xl(unsigned long long int value) { return value; }
+constexpr R_xlen_t operator""_xl(unsigned long long int value) { return value; }
 
 }  // namespace literals
 
